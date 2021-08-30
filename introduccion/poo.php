8bfb9b801a2706d00cf78d1eb2bfc577958eff31
MiguelGutierrez43 <mgutierrez@siancolors.cl>
2021-08-30 15:48:10 -0400

    insertamos comillas

--- introduccion/poo.php
+++ introduccion/poo.php
@@ -7,16 +7,16 @@ class Color{
 
     public function Ropa(){
 
-        echo "<p> Ropa de color Remera: $this ->remera, Gorra: $this ->gorra</p>";
+        echo "<p>Ropa de color Remera: $this ->remera, Gorra: $this ->gorra</p>";
     }
 }
 $a = new Color();
-$a -> remera = roja;
-$a -> gorra = azul;
+$a -> remera = "roja";
+$a -> gorra = "azul";
 $a -> Ropa();
 
 
 $b = new Color();
-$b -> remera = roja;
-$b -> gorra = azul;
+$b -> remera = "verde";
+$b -> gorra = "naranjo";
 $b -> Ropa();
