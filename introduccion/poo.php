042923e7e383912cb12b0d04d214bb460711c533
MiguelGutierrez43 <mgutierrez@siancolors.cl>
2021-08-30 15:32:19 -0400

    leccion poo

---
+++ introduccion/poo.php
@@ -0,0 +1,17 @@
+<?php
+
+class Color{
+
+    public $remera;
+    public $gorra;
+
+    public function Ropa(){
+
+        echo "<p> Ropa de color Remera: $this ->remera</p>";
+    }
+}
+
+a = new Color();
+a -> remera = roja;
+a -> gorra = azul;
+a -> Ropa();
