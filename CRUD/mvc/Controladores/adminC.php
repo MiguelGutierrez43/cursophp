2d88fa769afebb5aa8ef5f9be9353a978b1a2e5a
MiguelGutierrez43 <mgutierrez@siancolors.cl>
2021-08-31 15:51:34 -0400

    Ingreso al crud 2

---
+++ CRUD/mvc/Controladores/adminC.php
@@ -0,0 +1,16 @@
+<?php
+
+class AdminC{
+
+    public function IngresoC(){
+
+        if(isset($_POST["usuarioI"])){
+
+            $datosC = array("usuario"=>$_POST["usuarioI"],"clave"=>$_POST["claveI"]);
+
+            $tablaBD = "administradores";
+
+            $respuesta = AdminM::IngresoM($datosC,$tablaBD);
+        }
+    }
+}
