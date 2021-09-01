247f5f6453d0ddd497cd67d160f3d8ee6c5ab8bb
MiguelGutierrez43 <mgutierrez@siancolors.cl>
2021-09-01 16:17:18 -0400

    arreglo ultimo

--- CRUD/mvc/Controladores/empleadosC.php
+++ CRUD/mvc/Controladores/empleadosC.php
@@ -6,8 +6,8 @@ class EmpleadosC{
 
         if(isset($_POST["nombreR"])){
 
-            $datosC = array("nombre" => $_POST["nombreR"], "apellido" => $_POST["apellidoR"], "email" => $_POST["emailR"], "
-                puesto" => $_POST["puestoR"], "salario" => $_POST["salarioR"]);
+            $datosC = array("nombre" => $_POST["nombreR"], "apellido" => $_POST["apellidoR"], "email" => $_POST["emailR"], 
+                "puesto" => $_POST["puestoR"], "salario" => $_POST["salarioR"]);
 
             $tablaBD = "empleados";
 
@@ -15,7 +15,7 @@ class EmpleadosC{
 
             if($respuesta == "Bien"){
 
-                header ("location:index.php?ruta=empleados");
+                header("location:index.php?ruta=empleados");
 
             }else{
 
