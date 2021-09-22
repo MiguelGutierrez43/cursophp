6e2369edd209ff72275566426dbc73f7e0aff1a6
MiguelGutierrez43 <mgutierrez@siancolors.cl>
2021-09-22 16:14:35 -0300

    Ultimo Arreglo Actualizar

--- CRUD/mvc/Modelos/empleadosM.php
+++ CRUD/mvc/Modelos/empleadosM.php
@@ -65,12 +65,12 @@ class EmpleadosM extends ConexionBD{
         $pdo = ConexionBD::cBD()->prepare("UPDATE $tablaBD SET nombre = :nombre, apellido = :apellido, email = :email, 
         puesto = :puesto, salario = :salario WHERE id = :id");
 
-        $pdo -> bindParam(":id", $datos["id"], PDO::PARAM_INT);
-        $pdo -> bindParam(":nombre", $datos["nombre"], PDO::PARAM_STR);
-        $pdo -> bindParam(":apellido", $datos["apellido"], PDO::PARAM_STR);
-        $pdo -> bindParam(":email", $datos["email"], PDO::PARAM_STR);
-        $pdo -> bindParam(":puesto", $datos["puesto"], PDO::PARAM_INT);
-        $pdo -> bindParam(":salario", $datos["salario"], PDO::PARAM_STR);
+        $pdo -> bindParam(":id", $datosC["id"], PDO::PARAM_INT);
+        $pdo -> bindParam(":nombre", $datosC["nombre"], PDO::PARAM_STR);
+        $pdo -> bindParam(":apellido", $datosC["apellido"], PDO::PARAM_STR);
+        $pdo -> bindParam(":email", $datosC["email"], PDO::PARAM_STR);
+        $pdo -> bindParam(":puesto", $datosC["puesto"], PDO::PARAM_STR);
+        $pdo -> bindParam(":salario", $datosC["salario"], PDO::PARAM_STR);
 
         if($pdo -> execute()){
 
